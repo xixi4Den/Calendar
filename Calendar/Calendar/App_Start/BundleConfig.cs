9dabbe8ea5eefee42dadc9131102608b8e6d0505
DenisHihich <xixi4.den@gmail.com>
2015-06-13 12:43:07 +0300

    client side for home page

--- Calendar/Calendar/App_Start/BundleConfig.cs
+++ Calendar/Calendar/App_Start/BundleConfig.cs
@@ -4,22 +4,26 @@ namespace Calendar
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/libs").Include(
                         "~/Scripts/libs/jquery/jquery-{version}.js",
                         "~/Scripts/libs/jquery/jquery.validate*",
                         "~/Scripts/libs/bootstrap.js",
-                        "~/Scripts/libs/angular.js.js"));
+                        "~/Scripts/libs/angular.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
                         "~/Scripts/plugins/jquery.calendario.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/controllers/homePageController.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/calendar.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/calendar").Include(
+                    "~/Content/calendar.css",
                       "~/Content/custom_calendar.css"));
         }
     }
---
+++ Calendar/Calendar/Scripts/controllers/homePageController.js
@@ -0,0 +1,33 @@
+﻿angular.element(document).ready(function () {
+
+    angular.module('homePageApp', []).controller('homePageCtrl', function ($scope) {
+
+        $scope.calendar = angular.element('#calendar').calendario(
+            {
+                onDayClick: function($el, $content, dateProperties) {
+                    
+                }
+            }
+        );
+
+        $scope.switchToNextMonth = function() {
+            $scope.calendar.gotoNextMonth();
+        };
+
+        $scope.switchToPreviousMonth = function () {
+            $scope.calendar.gotoPreviousMonth();
+        };
+
+        $scope.switchToToday = function () {
+            $scope.calendar.gotoNow();
+        };
+
+        $scope.formattedMonthAndYear = function () {
+            var currentMonth = $scope.calendar.getMonthName();
+            var currentYear = $scope.calendar.getYear();
+            return currentMonth + " " + currentYear;
+        }
+    });
+
+    angular.bootstrap(angular.element('#homePageContent'), ['homePageApp']);
+});
